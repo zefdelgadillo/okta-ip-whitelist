e905ba41780aaa85c44e5470f04bfeb4ec91e6f1
Zef Delgadillo <zef@google.com>
2020-04-08 22:41:18 -0400

    Update for style/format

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 import urllib.request as request
 from googleapiclient.discovery import build
-import json 
+import json
 import uuid
 import os
 
-URL = os.environ.get('OKTA_URL') # "https://s3.amazonaws.com/ok4ta-ip-ranges/ip_ranges.json"
-TENANTS = os.environ.get('OKTA_TENANTS') # "us_cell_5,preview_cell_1"
+URL = os.environ.get(
+    'OKTA_URL')  # "https://s3.amazonaws.com/ok4ta-ip-ranges/ip_ranges.json"
+TENANTS = os.environ.get('OKTA_TENANTS')  # "us_cell_5,preview_cell_1"
 PROJECT = os.environ.get('GCP_PROJECT')
 
 compute = build('compute', 'v1')
+
 
 def main(event, context):
     print('Event: ' + str(event))
@@ -18,8 +20,9 @@ def main(event, context):
     tenant_list = TENANTS.split(",")
     print(raw_ip_ranges)
     for cell in tenant_list:
-        rule_name = cell.replace('_','-')
+        rule_name = cell.replace('_', '-')
         update_firewall(rule_name, raw_ip_ranges[cell]["ip_ranges"])
+
 
 def get_okta_ranges():
     try:
@@ -31,12 +34,14 @@ def get_okta_ranges():
         print(f'An error occured when trying to retrieve {URL}')
         raise
 
+
 def update_firewall(rule_name, ip_addresses):
-    body = {
-        'destinationRanges': ip_addresses
-    }
+    body = {'destinationRanges': ip_addresses}
     try:
-        request = compute.firewalls().patch(project=PROJECT,firewall=f'okta-{rule_name}',body=body,requestId=uuid.uuid4())
+        request = compute.firewalls().patch(project=PROJECT,
+                                            firewall=f'okta-{rule_name}',
+                                            body=body,
+                                            requestId=uuid.uuid4())
         response = request.execute()
         return response
     except Exception as e:
